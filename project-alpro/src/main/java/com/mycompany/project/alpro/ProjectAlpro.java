6e80672a053ad18a0474a0926302556308581ead
Fransiscus Xaverius <xaverius.fransiscus078@gmail.com>
2024-12-05 19:42:55 +0700

    feat: Changed project to maven project from NetBeans

---
+++ project-alpro/src/main/java/com/mycompany/project/alpro/ProjectAlpro.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project.alpro;
+
+/**
+ *
+ * @author xaver
+ */
+public class ProjectAlpro {
+
+    public static void main(String[] args) {
+        StartMenu LM = new StartMenu();
+        LM.setVisible(true);
+        LM.pack();
+        LM.setLocationRelativeTo(null);
+        LM.setResizable(false);
+    }
+}
--- projectalp/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module project.alp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens project.alp to javafx.fxml;
-    exports project.alp;
-}
